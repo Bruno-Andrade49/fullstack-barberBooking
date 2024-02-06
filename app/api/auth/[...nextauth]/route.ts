fa99b822328661cadbcd3279324075ec15f03ecd
Bruno-Andrade49 <b.emanueandrade@hotmail.com>
2024-02-06 11:59:37 -0300

    Adds bookings agend page

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,31 @@
-import NextAuth from "next-auth"
-import authOptions from "./options";
+import { db } from "@/app/_lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth, { NextAuthOptions } from "next-auth"
+import { Adapter } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+
+
+export const authOptions: NextAuthOptions = {
+
+    adapter: PrismaAdapter(db) as Adapter,
+    providers: [GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    })
+    ],
+    callbacks: {
+        async session({ session, user }) {
+            session.user = { ...session.user, id: user.id } as {
+                id: string;
+                name: string;
+                email: string;
+            };
+
+            return session;
+        }
+    }
+};
 
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST }
-export default authOptions
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -2,7 +2,7 @@ import { db } from "@/app/_lib/prisma"
 import BarberShopInfo from "./_components/barber-shop-info"
 import ServiceItem from "./_components/service-item"
 import { getServerSession } from "next-auth"
-import authOptions from "@/app/api/auth/[...nextauth]/route"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 
 interface BarbershopDetailsPageProps {
     params: {
